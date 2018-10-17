7d83e58e2bc12d22168840f5361c07026069c2fe
hungk1996a <hungtv@sphinx-software.com>
2018-10-17 11:00:39 +0700

    add Controller

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 const PhoneController = require('./Controller/PhoneController');
 const phoneController = new PhoneController();
 
-const knex = require('./config');
-
-const PhoneFactory = require('./phone/PhoneFactory');
-const PhoneRepository = require('./phone/PhoneRepository');
-
 app.post('/', (req, res, next) => phoneController.createPhone(req, res, next));
 
 app.get('/', (req, res, next) => phoneController.showDb(req, res, next));
